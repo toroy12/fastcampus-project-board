879c3b777dd0715833fbc35c9fe26c82b1c6cab9
toroy12 <toroy12@naver.com>
2023-05-22 20:58:36 +0900

    테스트에 auditing기능을 사용하기 위한 어노테이션 추가

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -23,6 +24,8 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 })
+
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class Article {
 
