b9dcb308998f46e4a609ff1c56daa8087d39a922
toroy12 <toroy12@naver.com>
2023-06-19 23:19:11 +0900

    댓글 서비스 로직의 테스트와 골격 잡기 댓글과 관련된 단순 crud 기능을 테스트로 표현 강의 시간 상 기능의 일부만 테스트로 표현함

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -3,7 +3,6 @@ package com.fastcampus.projectboard.repository;
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.QArticle;
 import com.querydsl.core.types.dsl.DateTimeExpression;
-import com.querydsl.core.types.dsl.SimpleExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
